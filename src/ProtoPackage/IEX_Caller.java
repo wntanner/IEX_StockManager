8321af8f2944341ba6d5d270bdbb91c805537589
wntanner <32973177+wntanner@users.noreply.github.com>
2018-11-04 14:36:54 -0600

    comment changes

--- src/ProtoPackage/IEX_Caller.java
+++ src/ProtoPackage/IEX_Caller.java
@@ -16,14 +16,12 @@ import java.net.URLConnection;
  */
 public class IEX_Caller {
 
-	// http://financials.morningstar.com/ajax/ReportProcess4CSV.html?t=TWTR&reportType=is&period=12&dataType=A&order=asc&columnYear=5&number=3
-	// TODO switch to a morningstar caller, read as csv ??? morningstar has
-	// different data ...
-
 	/*
 	 * Constants
 	 */
-	public static final String IEX_ATTRIBUTION = "Data provided for free by IEX... "; // View IEX's Terms of Use at https://iextrading.com/api-exhibit-a/ ... ";
+	public static final String IEX_ATTRIBUTION = "Data provided for free by IEX... "; // View IEX's Terms of Use at
+																						// https://iextrading.com/api-exhibit-a/
+																						// ... ";
 	public static final String UNKNOWN_SYMBOL_ERROR = "Unknown symbol"; // IEX's unknown symbol message
 	public static final String UNFINISHED_BUILD_ERROR = "Probelem with software - incomplete reading of stock information";
 	public static final String COMPLETED_CALL = "good";
@@ -32,8 +30,7 @@ public class IEX_Caller {
 	public static final String BASE_URL = "https://api.iextrading.com/1.0/stock/"; // + "/{Symbol}/book"
 
 	/**
-	 * Given a stock symbol, creates and returns a String
-	 * for the URL
+	 * Given a stock symbol, creates and returns a String for the URL
 	 * 
 	 * @param sym
 	 * @return
@@ -41,23 +38,22 @@ public class IEX_Caller {
 	public static String make_URL(String sym) {
 		return BASE_URL + sym + "/book";
 	}
-	
+
 	/*
-	 * Strings that correspond to various attributes in the JSON file
-	 * which IEX returns. TODO learn GSON
+	 * Strings that correspond to various attributes in the JSON file which IEX
+	 * returns. TODO learn GSON
 	 */
 	public static final String OPEN_MATCH = "\"open\""; // attribute for daily opening price of stock
 	public static final String CLOSE_MATCH = "\"close\""; // attribute for daily closing price of stock
 	public static final String NAME_MATCH = "\"companyName\"";
 	public static final String LATEST_PRICE_MATCH = "latestPrice";
 
-
 	public IEX_Caller() {
 	}
 
 	/**
-	 * Given a Stock : s, calls the IEX API
-	 * Fills the Stock's attributes with whatever IEX returned
+	 * Given a Stock : s, calls the IEX API Fills the Stock's attributes with
+	 * whatever IEX returned
 	 * 
 	 * @param s
 	 * @return
@@ -83,7 +79,7 @@ public class IEX_Caller {
 			this.fillStock(buffr, s);
 
 		}
-		
+
 		catch (MalformedURLException e) {
 			System.err.println("bad url - IEX_Caller.callAndFillStock");
 			e.printStackTrace();
@@ -93,7 +89,7 @@ public class IEX_Caller {
 
 			return IEX_Caller.UNKNOWN_SYMBOL_ERROR;
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
 			return ERROR + " with IO";
 		} finally {
@@ -107,8 +103,7 @@ public class IEX_Caller {
 			}
 		}
 		/*
-		 * Close is a tricky thing to get, but we solved.
-		 * We can remove this check.
+		 * Close is a tricky thing to get, but we solved. We can remove this check.
 		 */
 		if (s.getOpen() == null || s.getClose() == null)
 			return IEX_Caller.UNFINISHED_BUILD_ERROR;
@@ -120,10 +115,9 @@ public class IEX_Caller {
 	}
 
 	/**
-	 * Given a BufferedReader : buffr holding onto an InputStream 
-	 * from an IEX API call and a Stock, 
-	 * reads in information from buffr and fills up the Stock
-	 * with the stuff.
+	 * Given a BufferedReader : buffr holding onto an InputStream from an IEX API
+	 * call and a Stock, reads in information from buffr and fills up the Stock with
+	 * the stuff.
 	 * 
 	 * @param buffr
 	 * @param s
@@ -137,18 +131,17 @@ public class IEX_Caller {
 
 		if (ln.equals(IEX_Caller.UNKNOWN_SYMBOL_ERROR))
 			return false;
-		
+
 		// grab the first five lines of the file and split on comma ...
 		this.padWithLines(buffr, ln, 5);
 		String[] splitlines = ln.split(",");
-		
+
 		/*
-		 * ... so that we can start to search for matches
-		 * with the attributes we want
+		 * ... so that we can start to search for matches with the attributes we want
 		 */
 		for (int i = 0; i < splitlines.length - 1; i++) {
 			String cur = splitlines[i];
-//			System.out.println("current attribute checking is: " + cur);
+			// System.out.println("current attribute checking is: " + cur);
 
 			if (cur.contains(NAME_MATCH)) {
 				s.setName(cur.split(":")[1]);
@@ -169,9 +162,9 @@ public class IEX_Caller {
 		}
 		return true;
 	}
+
 	/**
-	 * Given a BufferedReader and String, sticks num number of lines
-	 * onto the String
+	 * Given a BufferedReader and String, sticks num number of lines onto the String
 	 * 
 	 * @param buffr
 	 * @param toPad
